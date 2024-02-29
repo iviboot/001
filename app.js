c76790236d2eb982e42ed8e31f0a9512334aec67
Ivan Almaraz BM <126647369+iviboot@users.noreply.github.com>
2024-02-29 15:35:51 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-
+var resultado = window.confirm('Eres mayor de edad?');
+if (resultado === true) {
+    window.alert('Okay, eres mayor de edad.');
+} else { 
+    window.alert('solicita a un mayor que te acompañe');
+}
