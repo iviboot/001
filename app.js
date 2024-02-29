7660bc39c02e34efd43fe9767d7d4204ef49f060
Ivan Almaraz BM <126647369+iviboot@users.noreply.github.com>
2024-02-29 17:20:51 -0600

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-var resultado = window.confirm('Eres mayor de edad?');
-if (resultado === true) {
-    window.alert('Okay, eres mayor de edad.');
-} else { 
-    window.alert('solicita a un mayor que te acompañe');
-}
