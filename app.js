ccf08ead95d0837145d96ffc1135331d90e67cd0
Ivan Almaraz BM <126647369+iviboot@users.noreply.github.com>
2024-03-13 16:04:31 -0600

    actualizacion

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 var resultado = window.confirm('Eres mayor de edad?');
+
+
+
+
+
+
+
 if (resultado === true) {
+
+
+
+
+
     window.alert('Okay, eres mayor de edad.');
+
+
+
+
+
 } else { 
+
+
+
+
+
     window.alert('solicita a un mayor que te acompañe');
+
+
+
+
 }
